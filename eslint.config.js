b06b075bd95f554555776c037d91524187ab4b95
DeanLuus22021994 <dean.luus22021994@gmail.com>
2025-06-04 10:46:37 +0200

    refactor: update ESLint configuration with Prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 // Flat config for ESLint (ESM, modern, future-proof)
+
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   js.configs.recommended,
@@ -10,13 +12,15 @@ export default [
       ecmaVersion: 2022,
       sourceType: 'module',
     },
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off',
       'no-debugger': 'warn',
       'prettier/prettier': 'error',
     },
-    plugins: {},
   },
   {
     files: ['**/src/tests/**/*.js', '**/__tests__/**/*.js'],
